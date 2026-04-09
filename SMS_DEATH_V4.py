220519f8f967c6f15cefdd7e8da44c1df9af04c8
kspop526-dot <kspop526@gmail.com>
2026-04-09 09:43:10 +0700

    Update SMS_DEATH_V4.py

--- SMS_DEATH_V4.py
+++ SMS_DEATH_V4.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 import aiohttp
 import time
 import os
-TOKEN = os.getenv('MTQ5MTA5MTY3MDYxODM0MTQxNg.GlBYpj.lFHeMbhKJsfgYsJWj3Yl98VzxLbWbZA01O8nIU')
-PREFIX = '!' 
+TOKEN = os.getenv('DISCORD_TOKEN')
+PREFIX = '!'
 
 intents = discord.Intents.default()
 intents.message_content = True 
